7b7660dc9e47431c478d1381f0824c14a1b64090
plenouveau <plenouveau@aliceandsmith.com>
2018-10-06 14:39:21 -0400

    Combat States + Stealth States

---
+++ LHGames/Bot/States/CombatState.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LHGames.Bot;
+using LHGames.Helper;
+
+internal class CombatState : State
+{
+    Player targetPlayer;
+    IPlayer playerInfo;
+    Point attackDestination;
+
+    public override void StartStates()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SetAttackDestination(Player newTargetPlayer)
+    {
+        targetPlayer = newTargetPlayer;
+        attackDestination = targetPlayer.Position;
+    }
+
+    protected override string UpdateState()
+    {
+        if(targetPlayer.Health > 0)
+        {
+            return AttackPlayer();
+        }
+        else
+        {
+            return ReturnHome();
+        }
+    }
+
+    string AttackPlayer()
+    {
+        if (Adjacent(attackDestination))
+        {
+            return AIHelper.CreateMeleeAttackAction(attackDestination);
+        }
+        else
+        {
+            return GoTo(attackDestination);
+        }
+    }
+}
+
+
---
+++ LHGames/Bot/States/StealthState.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LHGames.Bot;
+using LHGames.Helper;
+
+internal class StealthState : State
+{
+    Point stealthDestination;
+
+    public override void StartStates()
+    {
+    }
+
+    public void SetStealthDestination(Point newStealthDestination)
+    {
+        stealthDestination = newStealthDestination;
+    }
+
+    protected override string UpdateState()
+    {
+        if(brain.playerInfo.CarriedResources < brain.playerInfo.CarryingCapacity)
+        {
+            return StealthRessource();
+        }
+        else
+        {
+            return ReturnHome();
+        }
+    }
+
+    string StealthRessource()
+    {
+        if (Adjacent(stealthDestination))
+        {
+            return AIHelper.CreateStealAction(stealthDestination);
+        }
+        else
+        {
+            return GoTo(stealthDestination);
+        }
+    }
+}
+
